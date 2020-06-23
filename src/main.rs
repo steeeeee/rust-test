ff4de1f624182bd3a9c3132efa7efffbc0c0edee
steeeeee <stefano.giometti@gmail.com>
2020-06-23 16:09:02 +0200

    Simplifies prev program

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
-use std::str::from_utf8;
+use std::fs::read_to_string;
 
 fn main() {
-    let mut source_file = match File::open("hello.txt") {
-        Err(err) => panic!("Couldn't open: {}", err.description()),
-        Ok(value) => value
-    };
-
-    let stat = match source_file.metadata() {
-        Err(err) => panic!("Couldn't get stat from file: {}", err.description()),
-        Ok(value) => value
-    };
-
-    let mut buffer = vec![0; stat.len() as usize];
-
-    match source_file.read(&mut buffer) {
+    match read_to_string("hello.txt") {
         Err(err) => panic!("Couldn't read: {}", err.description()),
-        Ok(_) => ()
-    };
-
-    let data = match from_utf8(&buffer) {
-        Err(err) => panic!("Couldn't convert buffer to string: {}", err.description()),
-        Ok(value) => value
-    };
-
-    println!("Content is: {}", data);
+        Ok(data) => println!("Content is: {}", data)
+    }
 }
